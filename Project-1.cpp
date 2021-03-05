f9be23f51eba947b2a654ff6980ac2b5c8c0f9a5
rage <51074700+ragedevelopmentstudios@users.noreply.github.com>
2021-03-05 17:04:50 +1300

    Added Jukebox and Biggest

--- Project-1.cpp
+++ Project-1.cpp
@@ -329,6 +329,7 @@ int main()
 
                 else if (command == "operand")
                 {
+                    SetConsoleTitle(TEXT("YooConsole Operand Checker"));
                     int a, b, x, y;
 
                     cout << "Value A:";
@@ -351,6 +352,7 @@ int main()
                 }
                 else if (command == "guitest")
                 {
+                    SetConsoleTitle(TEXT("GUI Test"));
                     cout << "Sorry This Function Is Useless Because The Idiots Didnt Install The .NET Framework\n";
 
                     system("pause");
@@ -362,6 +364,7 @@ int main()
 
                 else if (command == "hjonk") // HJOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOK
                 {
+                    SetConsoleTitle(TEXT("Goose"));   
                     system("start chrome.exe https://cdn.discordapp.com/attachments/813243300626694195/814838411490885632/Goose.png https://www.youtube.com/watch?v=riJs7DlphEM");
                    
                     system("pause");
@@ -370,100 +373,101 @@ int main()
                     system("CLS");
                 }
                 else if (command == "god") // It Just Works What You - The ChalkEaters: 
-                //Watch this: our newest release
-                //The legendary series you've missed for years
-                //Comes back, get ready for some action
-                //Got couple bugs in here, some broken scripts right there
-                //The fans will fix it anyway so we don't care
-                //They'll patch it through micro transactions
-                //So enjoy your pretty nylon bag
-                //Oh and boy, stop shitting on our swag
-                //You're not getting any money back
-                //You knew that was a one way wacky track
-                //Just shut up and have your nylon bag
-                //Coming up with it was real drag
-                //All proceeds go strictly to Vault Tec
-                //So you're NOT getting your money back
-                //It just works, it just works
-                //Little lies, stunning shows
-                //People buy, money flows, it just works
-                //It just works, it just works
-                //Overpriced open worlds
-                //Earnings rise, take my word
-                //It just works
-                //It just works
-                //It just works
-                //It just works
-                //Our tech is cutting edge as far as I can tell
-                //The creation engine's aging very well
-                //We're not planning on doing anything about it
-                //Skyrim's the limit, don't deny it
-                //See this mountain ? You can buy it
-                //In the dlc for the price of a real mountain
-                //What's wrong with a train in place of your head?
-                //Come on, believe me, you've seen nothing yet
-                //The battle spaceship with the dragon animation
-                //Will become a new sensation on the internet
-                //Just shut up, give us your every dime
-                //Buying an armored horse is not a crime
-                //devs could eat the Shadowmere alive so
-                //Buy Skyrim one more Todd damn time
-                //It just works, it just works
-                //Little lies, stunning shows
-                //People buy, money flows, it just works
-                //It just works, it just works
-                //Overpriced open worlds
-                //Earnings rise, take my word
-                //It just works
-                //It just works
-                //It just works
-                //We don't want your endless shit
-                //It's just not the game we need
-                //Sick of looting useless junk
-                //Graphics making our eyes bleed
-                //We don't want your endless shit
-                //It's just not the game we need
-                //Sick of looting useless junk
-                //Graphics making our eyes bleed
-                //Now listen up
-                //Welcome to the place washed by Iliac waves
-                //There's two and a half peasants with the same ugly face
-                //You're not into small towns? Check out big-ass plains
-                //50 times the size of Skyrim's, twice as many pointless caves
-                //You see
-                //We at Bethesda know exactly what you want
-                //Hardcore gamers inspired us to improve shopping a lot
-                //Now each lockpick must be bought for an actual ten spot
-                //And saving now is a paid mod for just twenty a slot
-                //You're never getting bored, you'll have infinite quests
-                //There's every vagabond will put you to the test
-                //You'll be richly awarded with infinite chests
-                //Full of rusty swords and used pregnancy tests
-                //We got rid of skill trees, they were oldand lame
-                //Who needs a leveling system in a role playing game ?
-                //You came to slash and kill and save one more nation
-                //No more levels and skills, only degradation!
-                //It just works, it just works
-                //Little lies, stunning shows
-                //People buy, money flows, it just works
-                //It just works, it just works
-                //Overpriced open worlds
-                //Earnings rise, take my word
-                //It just works
-                //It just works, it just works
-                //Little lies, stunning shows
-                //People buy, money flows, it just works
-                //It just works, it just works
-                //Overpriced open worlds
-                //Earnings rise, take my word
-                //It just works
-                //It just works
-                //It just works
-                //It just works
-                //It just works
-                //It just works
-                //So, who's laughing now?
+                        //Watch this: our newest release
+                        //The legendary series you've missed for years
+                        //Comes back, get ready for some action
+                        //Got couple bugs in here, some broken scripts right there
+                        //The fans will fix it anyway so we don't care
+                        //They'll patch it through micro transactions
+                        //So enjoy your pretty nylon bag
+                        //Oh and boy, stop shitting on our swag
+                        //You're not getting any money back
+                        //You knew that was a one way wacky track
+                        //Just shut up and have your nylon bag
+                        //Coming up with it was real drag
+                        //All proceeds go strictly to Vault Tec
+                        //So you're NOT getting your money back
+                        //It just works, it just works
+                        //Little lies, stunning shows
+                        //People buy, money flows, it just works
+                        //It just works, it just works
+                        //Overpriced open worlds
+                        //Earnings rise, take my word
+                        //It just works
+                        //It just works
+                        //It just works
+                        //It just works
+                        //Our tech is cutting edge as far as I can tell
+                        //The creation engine's aging very well
+                        //We're not planning on doing anything about it
+                        //Skyrim's the limit, don't deny it
+                        //See this mountain ? You can buy it
+                        //In the dlc for the price of a real mountain
+                        //What's wrong with a train in place of your head?
+                        //Come on, believe me, you've seen nothing yet
+                        //The battle spaceship with the dragon animation
+                        //Will become a new sensation on the internet
+                        //Just shut up, give us your every dime
+                        //Buying an armored horse is not a crime
+                        //devs could eat the Shadowmere alive so
+                        //Buy Skyrim one more Todd damn time
+                        //It just works, it just works
+                        //Little lies, stunning shows
+                        //People buy, money flows, it just works
+                        //It just works, it just works
+                        //Overpriced open worlds
+                        //Earnings rise, take my word
+                        //It just works
+                        //It just works
+                        //It just works
+                        //We don't want your endless shit
+                        //It's just not the game we need
+                        //Sick of looting useless junk
+                        //Graphics making our eyes bleed
+                        //We don't want your endless shit
+                        //It's just not the game we need
+                        //Sick of looting useless junk
+                        //Graphics making our eyes bleed
+                        //Now listen up
+                        //Welcome to the place washed by Iliac waves
+                        //There's two and a half peasants with the same ugly face
+                        //You're not into small towns? Check out big-ass plains
+                        //50 times the size of Skyrim's, twice as many pointless caves
+                        //You see
+                        //We at Bethesda know exactly what you want
+                        //Hardcore gamers inspired us to improve shopping a lot
+                        //Now each lockpick must be bought for an actual ten spot
+                        //And saving now is a paid mod for just twenty a slot
+                        //You're never getting bored, you'll have infinite quests
+                        //There's every vagabond will put you to the test
+                        //You'll be richly awarded with infinite chests
+                        //Full of rusty swords and used pregnancy tests
+                        //We got rid of skill trees, they were oldand lame
+                        //Who needs a leveling system in a role playing game ?
+                        //You came to slash and kill and save one more nation
+                        //No more levels and skills, only degradation!
+                        //It just works, it just works
+                        //Little lies, stunning shows
+                        //People buy, money flows, it just works
+                        //It just works, it just works
+                        //Overpriced open worlds
+                        //Earnings rise, take my word
+                        //It just works
+                        //It just works, it just works
+                        //Little lies, stunning shows
+                        //People buy, money flows, it just works
+                        //It just works, it just works
+                        //Overpriced open worlds
+                        //Earnings rise, take my word
+                        //It just works
+                        //It just works
+                        //It just works
+                        //It just works
+                        //It just works
+                        //It just works
+                        //So, who's laughing now?
                 {
+                    SetConsoleTitle(TEXT("Todd Howard"));
                     system("start chrome.exe https://i.imgur.com/hguyOjZ.jpg https://www.youtube.com/watch?v=YPN0qhSyWy8");
 
                     cout << "Watch this: our newest release\nThe legendary series you've missed for years\nComes back, get ready for some action\nGot couple bugs in here, some broken scripts right there\nThe fans will fix it anyway so we don't care\nThey'll patch it through micro transactions\n\nSo enjoy your pretty nylon bag\nOh and boy, stop shitting on our swag\nYou're not getting any money back\nYou knew that was a one way wacky track\nJust shut up and have your nylon bag\nComing up with it was real drag\nAll proceeds go strictly to Vault Tec\nSo you're NOT getting your money back\n\nIt just works, it just works\nLittle lies, stunning shows\nPeople buy, money flows, it just works\nIt just works, it just works\nOverpriced open worlds\nEarnings rise, take my word\nIt just works\nIt just works\nIt just works\nIt just works\n\nOur tech is cutting edge as far as I can tell\nThe creation engine's aging very well\nWe're not planning on doing anything about it\nSkyrim's the limit, don't deny it\nSee this mountain ? You can buy it\nIn the dlc for the price of a real mountain\n\nWhat's wrong with a train in place of your head?\nCome on, believe me, you've seen nothing yet\nThe battle spaceship with the dragon animation\nWill become a new sensation on the internet\nJust shut up, give us your every dime\nBuying an armored horse is not a crime\nOur devs could eat the Shadowmere alive so\nBuy Skyrim one more Todd damn time\n\nIt just works, it just works\nLittle lies, stunning shows\nPeople buy, money flows, it just works\nIt just works, it just works\nOverpriced open worlds\nEarnings rise, take my word\nIt just works\nIt just works\nIt just works\n\nWe don't want your endless shit\nIt's just not the game we need\nSick of looting useless junk\nGraphics making our eyes bleed\nWe don't want your endless shit\nIt's just not the game we need\nSick of looting useless junk\nGraphics making our eyes bleed\n\nNow listen up\nWelcome to the place washed by Iliac waves\nThere's two and a half peasants with the same ugly face\nYou're not into small towns? Check out big-ass plains\n50 times the size of Skyrim's, twice as many pointless caves\n\nYou see\nWe at Bethesda know exactly what you want\nHardcore gamers inspired us to improve shopping a lot\nNow each lockpick must be bought for an actual ten spot\nAnd saving now is a paid mod for just twenty a slot\n\nYou're never getting bored, you'll have infinite quests\nThere's every vagabond will put you to the test\nYou'll be richly awarded with infinite chests\nFull of rusty swords and used pregnancy tests\n\nWe got rid of skill trees, they were oldand lame\nWho needs a leveling system in a role playing game ?\nYou came to slash and kill and save one more nation\nNo more levels and skills, only degradation!\n\nIt just works, it just works\nLittle lies, stunning shows\nPeople buy, money flows, it just works\nIt just works, it just works\nOverpriced open worlds\nEarnings rise, take my word\nIt just works\nIt just works, it just works\nLittle lies, stunning shows\nPeople buy, money flows, it just works\nIt just works, it just works\nOverpriced open worlds\nEarnings rise, take my word\nIt just works\nIt just works\nIt just works\nIt just works\nIt just works\nIt just works\n\nSo, who's laughing now?";
@@ -475,12 +479,104 @@ int main()
 
                 else if (command == "genius")
                 {
+                    SetConsoleTitle(TEXT("Genius"));
                     system("start chrome.exe https://i.kym-cdn.com/entries/icons/facebook/000/032/704/clarkson.jpg");
                     cout << "The Creator Of This Was Scott TB\n";
                     system("pause");
                     cout << "Press Any Key To Return To Menu";
                     reset = true;
                     system("CLS");
+                }
+
+                else if (command == "jukebox")
+                {
+                    SetConsoleTitle(TEXT("YooConsole JukeBox"));
+                    int track;
+                    cout << "What Song Do You Wnat To Play: \n1. Mr. Sandman \n2. It Just Works \n3. Mr. Blue Sky \n4. Fucked With An Anchor: \n Song Choice: ";
+                    cin >> track;
+
+                    switch (track)
+                    {
+                        case 1:
+                        {
+                            SetConsoleTitle(TEXT("YooConsole - Now Playing Mr. Sandman"));
+                            system("start chrome.exe https://www.youtube.com/watch?v=YGqdjaz2Upg");
+                            system("pause");
+                            cout << "Press Any Key To Return To Menu";
+                            reset = true;
+                            system("CLS");
+                            break;
+                        }
+                        case 2:
+                        {
+                            SetConsoleTitle(TEXT("YooConsole - It Just Works"));
+                            system("start chrome.exe https://www.youtube.com/watch?v=YPN0qhSyWy8");
+                            system("pause");
+                            cout << "Press Any Key To Return To Menu";
+                            reset = true;
+                            system("CLS");
+                            break;
+                        }
+                        case 3:
+                        {
+                            SetConsoleTitle(TEXT("YooConsole - Now Playing Mr. Blue Sky"));
+                            system("start chrome.exe https://www.youtube.com/watch?v=wuJIqmha2Hk");
+                            system("pause");
+                            cout << "Press Any Key To Return To Menu";
+                            reset = true;
+                            system("CLS");
+                            break;
+                        }
+                        case 4:
+                        {
+                            SetConsoleTitle(TEXT("YooConsole - Now Playing Fucked With An Anchor"));
+                            system("start chrome.exe https://www.youtube.com/watch?v=th4Czv1j3F8");
+                            system("pause");
+                            cout << "Press Any Key To Return To Menu";
+                            reset = true;
+                            system("CLS");
+                            break;
+                        }
+                    }
+                }
+
+                else if (command == "Biggest")
+                {
+                    float a, b, c;
+
+                    cout << "Input Value For A: ";
+                    cin >> a;
+
+                    cout << "Input Value For B: ";
+                    cin >> b;
+
+                    cout << "Input Value For C: ";
+                    cin >> c;
+
+                    if (a > b && a > c)
+                    {
+                        cout << "The Biggest Number Is: " << a;
+                        system("pause");
+                        cout << "\nPress Any Key To Return To Menu";
+                        reset = true;
+                        system("CLS");
+                    }
+                    else if (b > a && b > c)
+                    {
+                        cout << "the Biggest Number Is: " << b;
+                        system("pause");
+                        cout << "\nPress Any Key To Return To Menu";
+                        reset = true;
+                        system("CLS");
+                    }
+                    else if (c > b && c > a)
+                    {
+                        cout << "The Biggest Number Is: " << c;
+                        system("pause");
+                        cout << "Press Any Key To Return To Menu\n";
+                        reset = true;
+                        system("CLS");
+                    }
                 }
 
                 else
